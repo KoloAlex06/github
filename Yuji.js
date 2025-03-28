862ff0cf159e16dbd11a782f365f419436a65774
Kolo Alex <koloalexouattara@gmail.com>
2025-03-28 22:15:25 +0000

    Presentation du code

--- Yuji.js
+++ Yuji.js
@@ -1,3 +1,10 @@
 const birtheyears = prompt('Entrer votre année de naissance')
 const years = 2025
 const age = 2025 - birtheyears
+
+if(age >= 13){
+	console.log("Angelo")
+} else if(age < 13){
+	console.log("La casa de papel")
+} else if(age > 18){
+	console.log("Jujutsu keizen")
